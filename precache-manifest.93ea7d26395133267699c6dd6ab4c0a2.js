8843328018f8410fc49ad25b5d8047de62cef884
Cynthia <ivyc8192@gmail.com>
2019-05-03 23:54:13 -0700

    Updates

---
+++ precache-manifest.93ea7d26395133267699c6dd6ab4c0a2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d991a26c2b2c05c77975",
+    "url": "/static/css/main.97098a23.chunk.css"
+  },
+  {
+    "revision": "d991a26c2b2c05c77975",
+    "url": "/static/js/main.90b73006.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6ecff47a4db173510730",
+    "url": "/static/js/2.ac197cb6.chunk.js"
+  },
+  {
+    "revision": "8ee272a19ae74247229dab8f5bb2e007",
+    "url": "/static/media/logo.8ee272a1.png"
+  },
+  {
+    "revision": "9468851809668be548dddae5e8557b64",
+    "url": "/static/media/homepage.94688518.png"
+  },
+  {
+    "revision": "149266e6e90e2c4c4207c84a14d8adce",
+    "url": "/static/media/blueprint.149266e6.jpg"
+  },
+  {
+    "revision": "cff5933353f80b194499f954772d55e4",
+    "url": "/static/media/Warbler-cover.cff59333.png"
+  },
+  {
+    "revision": "fb5b3df935f96c3be2154f025ae025f3",
+    "url": "/static/media/Jobly-cover.fb5b3df9.png"
+  },
+  {
+    "revision": "84ea84764564bfaa636958759c5cf577",
+    "url": "/static/media/Microblog-cover.84ea8476.png"
+  },
+  {
+    "revision": "e3a457571441824f20d2ac4d0e45c9a9",
+    "url": "/static/media/resume.e3a45757.pdf"
+  },
+  {
+    "revision": "fd8326ab15079b3a0af64f13aa514975",
+    "url": "/index.html"
+  }
+];
